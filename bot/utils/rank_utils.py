76b31019bee544a401b78b91011a3c3415ade27d
BOP PC <provers.0-pluses@icloud.com>
2025-01-27 21:52:52 +0000

    fixed player_ranks.json appearing in main folder

--- bot/utils/rank_utils.py
+++ bot/utils/rank_utils.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # JSON file for storing player ranks
-RANKS_FILE = "player_ranks.json"
+RANKS_FILE = "bot/player_ranks.json"
 
 def load_ranks():
     """Load ranks from a JSON file."""
