21ee5b2aaad1ab76375cdcc9540c75d606a8ad4f
BOP PC <provers.0-pluses@icloud.com>
2025-03-17 22:19:06 +0000

    Refactor WOMtest.py: Enhance logging, load configuration, and initialize WOM client for group details fetching

--- WOMtest.py
+++ WOMtest.py
@@ -1,13 +1,72 @@
-WOMtest.py
-
+# WOMtest.py
 # A test script to gather data from the Wise Old Man API
 
-import requests
+from wom import Client
+import configparser
+import os
+from datetime import datetime
+import asyncio
+from discord.ext import tasks
+import sys
+# ------------------------------------------------------------------------------
+# Helper Functions
+# ------------------------------------------------------------------------------
 
-# Define the API endpoint
-url = "https://api.wiseoldman.net/v1/groups/1234567890"
+old_stdout = sys.stdout
+log_file = open("log.txt", "w")
+sys.stdout = log_file
 
-# Send a GET request to the API endpoint
-response = requests.get(url)
+
+def log(message: str):
+    """Logs a message with the current timestamp."""
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_message = f"{timestamp} - {message}"
+    print(formatted_message)  # Print to terminal
+
+# ------------------------------------------------------------------------------
+# Configuration Loading
+# ------------------------------------------------------------------------------
+
+config = configparser.ConfigParser()
+config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+config.read(config_file)
+
+
+# Fetch settings from config.ini	
+
+channel_id          = int(config['discord']['channel_id'])
+group_id            = int(config['wiseoldman']['group_id'])
+group_passcode      = config['wiseoldman']['group_passcode']
+
+# ------------------------------------------------------------------------------
+# Wise Old Man Client Initialization
+# ------------------------------------------------------------------------------
+
+wom_client = Client()
+
+
+async def main():
+    # Start the WOM client session
+    await wom_client.start()
+    
+    # Fetch group details
+    result = await wom_client.groups.get_details(group_id)
+    sys.stdout = old_stdout
+    log_file.close()
+    print("Group details fetched successfully")
+    
+    # Close the client session when done
+    await wom_client.close()
+
+asyncio.run(main())
+
+
+
+
+
+
+
+
+
 
 
