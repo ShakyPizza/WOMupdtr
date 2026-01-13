454c2c88d8f7a7a93d1a790359958a8d38645acc
ShakyPizza <105827753+ShakyPizza@users.noreply.github.com>
2026-01-13 22:46:16 +0000

    fix: update Baserow token key retrieval to use fallback option

--- python/utils/baserow_connect.py
+++ python/utils/baserow_connect.py
@@ -9,7 +9,7 @@ config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 config.read(config_file)
 
 
-token = config['baserow']['br_token']
+token = config.get('baserow', 'br_token', fallback=None)
 
 if not token:
     raise ValueError("Baserow token is not set in the config.ini file.")
